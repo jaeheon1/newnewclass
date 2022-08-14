d81b8a5ccdc383b6752c7544931cfd0a9bca21d9
jaeheon1 <jungjaehen@naver.com>
2022-08-14 22:04:06 +0900

    continue

--- newunitys/Assets/Scenes/PlayBall.cs
+++ newunitys/Assets/Scenes/PlayBall.cs
@@ -4,18 +4,44 @@ using UnityEngine;
 
 public class PlayBall : MonoBehaviour
 {
-    
+    public float JumpPower;
+    bool isJump;
+
+
+    Rigidbody rigid;
+
     void Awake()
     {
+        isJump = false;
+        rigid = GetComponent<Rigidbody>();
 
     }
 
-
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump")&& !isJump)
+        {
+            isJump = true;  
+            rigid.AddForce(new Vector3(0, JumpPower, 0), ForceMode.Impulse);
+        }
+    }
 
 
     void FixedUpdate()
     {
-        
 
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        rigid.AddForce(new Vector3(h, 0, v),ForceMode.Impulse);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Floor")
+        {
+            isJump = false;
+        }
     }
 }
